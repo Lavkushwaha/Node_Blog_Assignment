302bb3a9e5e41036747217cfb08412aa28e00984
unknown <kush373@gmail.com>
2019-06-05 13:52:23 -0700

    Express Routes added

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://lavkush:Lav@123@cluster0-fnbnt.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+const express = require('express');
+const app = express();
+
+//env.port this port is automatically assigned by heroku or deployment platform
+var port = process.env.PORT || 8080;
+
+var data;
+
+//connect to mongo db atlas
 client.connect(err => {
-   var data = client.db("sample_airbnb").collection("listingsAndReviews").find();
+    data = client.db("sample_airbnb").collection("listingsAndReviews");
   // perform actions on the collection object
   console.log(data);
   client.close();
 });
+
+//routing setup for express
+app.get('/', (req, res) => res.send('Hello !!'));
+app.listen(port, () => console.log(`Blog App listening on port ${port}!`));
