03b8d7620d2fea9093803fe17f3e331ca3f4762a
unknown <kush373@gmail.com>
2019-06-06 21:19:21 -0700

    error creating user model

---
+++ models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+const UserSchema = new mongoose.Schema({
+  username: {type: String, required: true, index: {unique: true}},
+  email: {type: String, required: true, index: {unique: true}, match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email']},
+  password: {type: String, required: true}
+});
+
+UserSchema.plugin(uniqueValidator, {message: 'username or email exists'});
+
+module.exports = mongoose.model('User', UserSchema, 'users');
